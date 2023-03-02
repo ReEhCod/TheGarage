3739acbf98ddb75fbf56035c30d086f63578a52d
ReEhCod <ehsani.reza95@yahoo.se>
2023-03-03 00:09:12 +0100

    Update CarRepo
    Added some mothods to the CarRepository.

--- TheGarage/Services/CarRepo.cs
+++ TheGarage/Services/CarRepo.cs
@@ -1,12 +1,40 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Navigation;
+using TheGarage.Data;
+using TheGarage.Models;
 
 namespace TheGarage.Services
 {
     public class CarRepo
     {
+        // Making a readonly instance of AppDbContext
+        private readonly AppDbContext _context;
+        public CarRepo(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Get one car by its id
+        public CarModel? GetCar(int id)
+        {
+            return _context.Cars.FirstOrDefault(c => c.CarId == id);
+        }
+        public List<CarModel> GetCars()
+        {
+            return _context.Cars.ToList();
+        }
+
+        // Get all cars 
+        public async Task<List<CarModel>> GetCarsAsync()
+        {
+            return await _context.Cars.ToList();
+        }
+
+
     }
 }
